91d0f73bdc3f0b4d6e96b382a2d96cd98ecd234f
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-06 00:00:16 -0500

    showing current temperature

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
   let tempertureElement = document.querySelector("temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   tempertureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
