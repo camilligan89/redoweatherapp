c01531409ece503f065938f8432389f3f2a17012
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-05 23:46:56 -0500

    js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 function displayTemperature(response) {
   let tempertureElement = document.querySelector("temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   tempertureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
