1b80bfcb55b7bfa9ea4cea0a6575ab84e968419f
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-05 23:23:39 -0500

    working on the temperature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+function displayTemperature(response) {
+  let tempertureElement = document.querySelector("temperature");
+  tempertureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+axious.get(apiurl).then(displayTemperature);
