585ac48c95940847985b38bdc8ed8d2c6e8d96cb
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-13 14:15:38 -0500

    reverse url

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/10d@2x.png`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`
   );
 }
 
