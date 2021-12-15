365533c3c4b66ddd4398de97b0738d0e68a5918f
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-15 11:52:31 -0500

    correct

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`
   );
 }
 
